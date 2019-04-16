96117b92d202e135f32198040b5eaa99935c3c6b
Morteza <mortezaafg1011@gmail.com>
2019-04-16 10:37:23 +0430

    adding first version of barnama template

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+})
+
+require('bootstrap.min');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+//
+// Vue.component('register-component', require('./components/Auth/Register.vue'));
+// Vue.component('login-component', require('./components/Auth/Login.vue'));
+// Vue.component('reset-password-component', require('./components/Auth/ResetPassword.vue'));
+// Vue.component('profile-edit-component', require('./components/ProfileEdit.vue'));
+// Vue.component('cargo-request-create', require('./components/Driver/CargoRequestCreate.vue'));
+// Vue.component('cargo-request-edit', require('./components/Driver/CargoRequestEdit.vue'));
+// Vue.component('cargo-create', require('./components/Owner/CargoCreate'));
+// Vue.component('cargo-edit', require('./components/Owner/CargoEdit'));
+// Vue.component('list-filter', require('./components/ListFilter'));
+// Vue.component('item-list', require('./components/Home/ItemList'));
+// Vue.component('cargo-and-request-form', require('./components/CargoAndRequestCreateForm'));
+// Vue.component('contact', require('./components/Pages/Contact'));
+//
+// const app = new Vue({
+//     el: '#app',
+//
+//     data: {
+//     },
+//
+//     methods: {
+//     }
+// });
